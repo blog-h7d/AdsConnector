b5de29068ec564bdcc3de025282841fdae30a403
mh7d <66883304+mh7d@users.noreply.github.com>
2021-12-27 17:51:05 +0100

    initial web page

--- config_manager.py
+++ config_manager.py
@@ -7,7 +7,7 @@ CONFIG_FILE_PATH = os.path.abspath(os.path.join('', "config.json"))
 
 async def init_config_file(delete_act_config=False):
     config_file_path = await get_config_file_path()
-    if delete_act_config:
+    if not os.path.exists(config_file_path) or delete_act_config:
         if os.path.exists(config_file_path):
             os.remove(config_file_path)
 
@@ -22,20 +22,23 @@ async def init_config_file(delete_act_config=False):
             config_file.write(quart.json.dumps(default_data))
 
 
-async def get_config_data(configfile=""):
-    if not configfile:
-        configfile = CONFIG_FILE_PATH
-    config_file_path = configfile
+async def get_config_data(configfile: str = ""):
+    await init_config_file()
+
+    config_file_path = await get_config_file_path()
     with open(config_file_path) as config_file:
         text = config_file.read()
         json_obj = quart.json.loads(text)
         return json_obj
 
 
-async def get_config_value(key: str):
+async def get_config_value(key: str, default: str = None):
     data = await get_config_data()
     if key in data:
         return data[key]
+
+    if default:
+        return default
 
     raise ValueError('Key ' + key + ' not in config data')
 
--- controller.py
+++ controller.py
@@ -1,7 +1,31 @@
 import quart
 
+import config_manager
+
 app = quart.Quart(__name__)
+# TODO: Make this accessible from Docker
 app.secret_key = "AppForPyADS_ChangeForUsage"
+
+
+@app.route('/connection/check/')
+async def check_connection():
+    return ''
+
+
+@app.route('/command/check/<id>/')
+async def check_command(id: str):
+    return ''
+
+
+@app.route("/")
+async def main():
+    data = {
+        'adsserver': await config_manager.get_config_value('adsserver'),
+        'amsnetid': await config_manager.get_config_value('amsnetid'),
+        'port': await config_manager.get_config_value('port', default='851'),
+    }
+    return await quart.render_template('main.html', data=data)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)

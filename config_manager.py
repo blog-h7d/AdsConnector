c1fd541d7d3a20d04bed48171e2f0d9d5f7d1ebc
mh7d <66883304+mh7d@users.noreply.github.com>
2021-12-28 16:42:48 +0100

    add save config to config manager refactoring

--- config_manager.py
+++ config_manager.py
@@ -1,55 +1,56 @@
 import os
+import typing
 
 import quart
 
 CONFIG_FILE_PATH = os.path.abspath(os.path.join('', "config.json"))
 
 
-async def init_config_file(delete_act_config=False):
-    config_file_path = await get_config_file_path()
-    if not os.path.exists(config_file_path) or delete_act_config:
-        if os.path.exists(config_file_path):
-            os.remove(config_file_path)
+class ConfigManager:
 
+    def __init__(self, delete_act_config=False):
+        config_file_path = self.get_config_file_path()
+        if not os.path.exists(config_file_path) or delete_act_config:
+            if os.path.exists(config_file_path):
+                os.remove(config_file_path)
+
+            with open(config_file_path, "w") as config_file:
+                default_data = {
+                    "adsserver": "",
+                    "amsnetid": "",
+                    "port": "851",
+                    "commands": list(),
+                    "writecommands": list()
+                }
+                config_file.write(quart.json.dumps(default_data))
+
+    async def get_config_data(self):
+        config_file_path = self.get_config_file_path()
+        with open(config_file_path) as config_file:
+            return quart.json.load(config_file)
+
+    async def get_config_value(self, key: str, default: str = None):
+        data = await self.get_config_data()
+        if key in data:
+            return data[key]
+
+        if default:
+            return default
+
+        raise ValueError('Key ' + key + ' not in config data')
+
+    async def _write_config_file(self, config_data):
+        config_file_path = self.get_config_file_path()
         with open(config_file_path, "w") as config_file:
-            default_data = {
-                "adsserver": "",
-                "amsnetid": "",
-                "port": "851",
-                "commands": list(),
-                "writecommands": list()
-            }
-            config_file.write(quart.json.dumps(default_data))
+            config_file.write(quart.json.dumps(config_data))
+            config_file.flush()
+            config_file.close()
 
+    async def save_entry(self, key: str, value: typing.Union[str, list]):
+        data = await self.get_config_data()
+        data[key] = value
+        await self._write_config_file(data)
 
-async def get_config_data(configfile: str = ""):
-    await init_config_file()
-
-    config_file_path = await get_config_file_path()
-    with open(config_file_path) as config_file:
-        text = config_file.read()
-        json_obj = quart.json.loads(text)
-        return json_obj
-
-
-async def get_config_value(key: str, default: str = None):
-    data = await get_config_data()
-    if key in data:
-        return data[key]
-
-    if default:
-        return default
-
-    raise ValueError('Key ' + key + ' not in config data')
-
-
-async def write_config_file(config_data):
-    config_file_path = await get_config_file_path()
-    with open(config_file_path, "w") as config_file:
-        config_file.write(quart.json.dumps(config_data))
-        config_file.flush()
-        config_file.close()
-
-
-async def get_config_file_path():
-    return CONFIG_FILE_PATH
+    @staticmethod
+    def get_config_file_path():
+        return CONFIG_FILE_PATH
--- test/test_config_manager.py
+++ test/test_config_manager.py
@@ -1,32 +1,50 @@
+import json
+
 import pytest
 
 import config_manager
 
 
-@pytest.mark.asyncio
-async def test_get_config_data():
-    await config_manager.init_config_file(True)
-    value = await config_manager.get_config_value('adsserver')
-    assert isinstance(value, str)
+class TestConfigManager:
 
+    @pytest.mark.asyncio
+    async def test_init_(self):
+        cm = config_manager.ConfigManager()
+        assert cm
 
-@pytest.mark.asyncio
-async def test_get_invalid_config_data():
-    await config_manager.init_config_file(True)
-    with pytest.raises(ValueError):
-        await config_manager.get_config_value('host')
+    @pytest.mark.asyncio
+    async def test_get_config_data(self):
+        cm = config_manager.ConfigManager(True)
+        value = await cm.get_config_value('adsserver')
+        assert isinstance(value, str)
 
+    @pytest.mark.asyncio
+    async def test_get_invalid_config_data(self):
+        cm = config_manager.ConfigManager(True)
+        with pytest.raises(ValueError):
+            await cm.get_config_value('host')
 
-@pytest.mark.asyncio
-async def test_config_default_content():
-    await config_manager.init_config_file(True)
-    value = await config_manager.get_config_value('commands')
-    assert isinstance(value, dict)
+    @pytest.mark.asyncio
+    async def test_config_default_content(self):
+        cm = config_manager.ConfigManager(True)
+        value = await cm.get_config_value('commands')
+        assert isinstance(value, list)
 
+    @pytest.mark.asyncio
+    async def test_get_config_data(self):
+        cm = config_manager.ConfigManager(True)
+        data = await cm.get_config_data()
+        assert 'adsserver' in data
+        assert 'port' in data
 
-@pytest.mark.asyncio
-async def test_get_config_data():
-    await config_manager.init_config_file(True)
-    data = await config_manager.get_config_data()
-    assert 'adsserver' in data
-    assert 'port' in data
+    @pytest.mark.asyncio
+    async def test_save_config_entry(self):
+        cm = config_manager.ConfigManager(True)
+        await cm.save_entry('adsserver', '192.168.0.1')
+
+        fp = cm.get_config_file_path()
+        with open(fp) as json_file:
+            data = json.load(json_file)
+
+        assert 'adsserver' in data
+        assert data['adsserver'] == '192.168.0.1'
